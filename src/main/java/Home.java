15463049eb95ce785e516f09ce6f42d7e7a16b05
1-MBrain <1-MBrain@BH-R20-04.local.gfmat.org>
2023-09-29 12:29:19 +0100

    database access

--- src/main/java/Home.java
+++ src/main/java/Home.java
@@ -1,3 +1,6 @@
+
+import com.DatabaseAccess;
+
 //Mr Lees can now see this
 
 /*
@@ -29,30 +32,55 @@ public class Home extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
+        btnTestConnection = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setText("Home Menu");
+
+        btnTestConnection.setText("Test Connection");
+        btnTestConnection.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnTestConnectionActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(157, 157, 157)
-                .addComponent(jLabel1)
-                .addContainerGap(187, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(159, 159, 159)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(btnTestConnection)))
+                .addContainerGap(185, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
-                .addContainerGap(275, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(btnTestConnection)
+                .addContainerGap(234, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnTestConnectionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTestConnectionActionPerformed
+        // TODO add your handling code here:
+        boolean connection = DatabaseAccess.sqlTestDBConnection();
+        if (connection == true) {
+            System.out.println("Connection made, gj.");
+        } else {
+            System.out.println("lol unlucky loser");
+        }
+    }//GEN-LAST:event_btnTestConnectionActionPerformed
 
     /**
      * @param args the command line arguments
@@ -90,6 +118,7 @@ public class Home extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnTestConnection;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
